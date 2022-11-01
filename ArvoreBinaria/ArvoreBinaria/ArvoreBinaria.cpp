99afb57709c3473b6caa3ed6fb41ab55f4bd013c
Guilherme  Franco <106282119+gdkbr@users.noreply.github.com>
2022-11-01 10:41:25 -0300

    Update ArvoreBinaria.cpp

--- ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
+++ ArvoreBinaria/ArvoreBinaria/ArvoreBinaria.cpp
@@ -79,7 +79,7 @@ void menu()
 void inicializar()
 {
 
-	// provisÛrio porque n„o libera a memoria usada pela arvore
+	// provis√≥rio porque n√£o libera a memoria usada pela arvore
 	NO* raiz = NULL;
 
 	cout << "Arvore inicializada \n";
@@ -177,8 +177,25 @@ void exibirElementosArvore(NO* no)
 
 void buscarElementoArvore(NO* no, int valor)
 {
-	
+	if (no == NULL) {
+		cout << "o numero nao foi encontrado." << endl;
+		return;
+	}
 
+	else {
+		if (valor == no->valor) {
+			cout << "o numero foi encontrado" << endl;
+		}
+
+		else {
+			if (valor < no->valor) {
+				buscarElementoArvore(no->esq,valor);
+			}
+			else {
+				buscarElementoArvore(no->dir, valor);
+			}
+		}
+	}
 }
 
 
